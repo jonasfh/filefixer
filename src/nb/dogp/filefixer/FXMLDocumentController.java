6a1d9d6506715ab33885afff6936c1fb27366c0b
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2017-10-31 22:27:31 +0100

    Refactored: Moved code to package nb.dogp.filefixer

--- src/nb/dogp/filefixer/FXMLDocumentController.java
+++ src/nb/dogp/filefixer/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package filefixer;
+package nb.dogp.filefixer;
 
 import java.io.File;
 import java.io.FileInputStream;
--- src/nb/dogp/filefixer/FileFixer.java
+++ src/nb/dogp/filefixer/FileFixer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package filefixer;
+package nb.dogp.filefixer;
 
 import java.net.URL;
 import java.util.logging.Level;
